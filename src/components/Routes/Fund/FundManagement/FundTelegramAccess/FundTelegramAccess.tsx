5e9b872c96c9b1a5ea33fa0e95fec59681420a9a
ishak-lm <leghlamishak@gmail.com>
2020-02-28 14:57:18 +0100

    [WIP] Send data to server

--- src/components/Routes/Fund/FundManagement/FundTelegramAccess/FundTelegramAccess.tsx
+++ src/components/Routes/Fund/FundManagement/FundTelegramAccess/FundTelegramAccess.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as Yup from 'yup';
 import { useForm, FormContext } from 'react-hook-form';
 import { Block, BlockActions } from '~/storybook/components/Block/Block';
 import { SectionTitle } from '~/storybook/components/Title/Title';
 import { Input } from '~/storybook/components/Input/Input';
 import { Button } from '~/storybook/components/Button/Button';
+import { useEnvironment } from '~/hooks/useEnvironment';
+import { useAccount } from '~/hooks/useAccount';
 
 export const FundTelegramAccess: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const environment = useEnvironment()!;
+  const account = useAccount();
+
   const validationSchema = Yup.object().shape({
-    telegramId: Yup.string(),
+    telegramId: Yup.string().required(),
   });
 
   const defaultValues = {
@@ -21,8 +27,35 @@ export const FundTelegramAccess: React.FC = () => {
     mode: 'onSubmit',
   });
 
-  const submit = form.handleSubmit(data => {
-    console.log(data);
+  const submit = form.handleSubmit(async data => {
+    try {
+      setLoading(true);
+
+      const signedTransaction = await environment?.client.sign(
+        `I am the manager of the Melon fund ${account.fund}`,
+        account.address!
+      );
+
+      const response = await fetch('http://localhost:8080/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          fund: account.fund,
+          address: account.address,
+          telegramId: data.telegramId,
+          data: `I am the manager of the Melon fund ${account.fund}`,
+          signedTransaction: signedTransaction,
+        }),
+      });
+
+      console.log(await response.json());
+
+      form.setValue('telegramId', '');
+      setLoading(false);
+    } catch (e) {
+      console.error(e);
+      setLoading(false);
+    }
   });
 
   return (
@@ -32,7 +65,9 @@ export const FundTelegramAccess: React.FC = () => {
         <form onSubmit={submit}>
           <Input id="telegramId" name="telegramId" placeholder="Enter your telegram ID" type="text" />
           <BlockActions>
-            <Button type="submit">Get Access</Button>
+            <Button type="submit" loading={loading}>
+              Get Access
+            </Button>
           </BlockActions>
         </form>
       </FormContext>
